017b650f8f9ecd7ac722d946f251f32e7a2b6ee6
godfindapu@gmail.com <Universe2020#>
2021-10-10 02:58:38 +0530

    CoreActivity - HelloWorld

--- BaseJava/src/main/java/com/apu/basejava/CoreActivity.java
+++ BaseJava/src/main/java/com/apu/basejava/CoreActivity.java
@@ -1,6 +1,13 @@
 package com.apu.basejava;
 
+import android.widget.Toast;
+
 import androidx.appcompat.app.AppCompatActivity;
 
 abstract public class CoreActivity extends AppCompatActivity {
+
+    protected void HelloWorld(){
+        Toast.makeText(this, "Hello World", Toast.LENGTH_SHORT).show();
+    }
+
 }
